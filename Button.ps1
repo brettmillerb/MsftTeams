2ab3dded7b96afe2bbcc4a282351e981319b2c2d
Brett Miller <brett@millerb.co.uk>
2018-02-26 13:12:47 +0000

    Added Button Help and updated ReadMe to include the new button types

--- Button.ps1
+++ Button.ps1
@@ -1,4 +1,37 @@
 function Button {
+    <#
+    .SYNOPSIS
+    Creates a Button JSON object for use with MSTeams Module
+    
+    .DESCRIPTION
+    Creates a Button Json object for use with MSTeams New-TeamsMessage function.
+    This dynamically generates JSON for different button types based on the parameters passed
+    
+    .PARAMETER ButtonType
+    The type of button to be used, there are four different types
+    TextInput, DateInput, HttpPOST, OpenURI
+    
+    .PARAMETER ButtonName
+    The text to appear on the button to be pressed
+    
+    .PARAMETER TargetURI
+    The target URL that the button opens or attempts to POST a message.
+    
+    .EXAMPLE
+    Button -ButtonType TextInput -ButtonName 'Leave a Comment' -TargetURI 'https://millerb.co.uk'
+
+    .EXAMPLE
+    Button -ButtonType DateInput -ButtonName 'Choose a Date' -TargetURI 'https://millerb.co.uk'
+
+    .EXAMPLE
+    Button -ButtonType HttpPost -ButtonName 'POST Stuff' -TargetURI 'https://millerb.co.uk'
+
+    .EXAMPLE
+    Button -ButtonType OpenURI -ButtonName 'Open Me :)' -TargetURI 'https://millerb.co.uk'
+    
+    .NOTES
+    General Notes
+    #>
     Param (
         [Parameter(Mandatory = $true,
             Position = 0)]
